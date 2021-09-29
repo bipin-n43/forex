880fbb8e92a70af93fe25276e8b5af0826f9482c
Bipin Nepal <bipinnepal43@gmail.com>
2021-09-29 23:43:55 +0545

    minor changes on forex controller

--- src/main/java/com/microservice/forex/contoller/ForexController.java
+++ src/main/java/com/microservice/forex/contoller/ForexController.java
@@ -1,25 +1,31 @@
 package com.microservice.forex.contoller;
-
 import com.microservice.forex.entity.ExchangeValue;
 import com.microservice.forex.repository.ExchangeValueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 @RestController
 public class ForexController {
-    @Resource
+
+    @Autowired
     private Environment environment;
-    @Resource
-     private ExchangeValueRepository exchangeValueRepository;
+
+    @Autowired
+    private ExchangeValueRepository repository;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
-        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from,to);
-        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+    public ExchangeValue retrieveExchangeValue
+            (@PathVariable String from, @PathVariable String to){
+
+        ExchangeValue exchangeValue =
+                repository.findByFromAndTo(from, to);
+
+        exchangeValue.setPort(
+                Integer.parseInt(environment.getProperty("local.server.port")));
+
         return exchangeValue;
     }
 }
